0935dd8dba49617565b45b7f993698245b7fc31b
nurvitafni <officiallyafni@gmail.com>
2019-08-07 11:16:53 +0700

    Coba

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+// app.get('/', (req, res) => res.send('Hello world'))
+
+app.get('/', (req, res) => {
+    var x, y, z;
+    x = 7;
+    y = 8;
+    z = x + y;
+
+    return res.send("The value of z is " + z + ".")
+})
+
+app.post('/', (req, res) => {
+    let email = req.body.email
+
+    return res.send('The email value is ' + email)
+})
+
+app.listen(8002, () => {
+    console.log('Example app listening to port 8001')
+})
