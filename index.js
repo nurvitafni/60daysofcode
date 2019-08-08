ec250493e78d64fb23bd4e91b5b6ec85a09f7062
nurvitafni <officiallyafni@gmail.com>
2019-08-08 13:40:04 +0700

    New uploaded files.

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const array_sort = require("./lib/array-sort")
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// app.get('/', (req, res) => {
+// 	let buah = ["Mangga", "Jeruk", "Pisang", "Anggur", "Nanas"]
+
+// 	return res.send("Urutan nama buah secara ascending: " + buah.sort())
+// })
+
+app.get('/task1', (req, res) => {
+	let warna = ["Purple", "Blue", "Yellow", "Green"]
+	
+	let hasil = array_sort.ascending_sort(warna)
+
+	return res.send(hasil)
+
+})
+
+app.get('/task2', (req, res) => {
+	let warna = ["Purple", "Blue", "Yellow", "Green"]
+
+	let hasil = array_sort.descending_sort(warna)
+
+	return res.send(hasil)
+
+})
+
+app.listen(3300, () => {
+    console.log(`Example app listening on port 3300`)
+})
